0de9b2ec53589a3aaa1712855189ff73ed61e77a
KatWest <katherine.west@bellevuecollege.edu>
2025-11-28 07:34:26 -0800

    Basic PlayerManagementService complete

---
+++ PlayerManagementService/Model/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayerManagementService.Model
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; } = string.Empty;
+        public bool IsDrafted { get; set; } = false;
+    }
+}
