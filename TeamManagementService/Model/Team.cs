06da4c187fee8740e87de21a7a5ceac570406fc6
Falllstreak <23.anomaly@gmail.com>
2025-12-04 19:40:03 -0800

    Edwin P. - Team Management Service created. 12/4/25 7:40 PM

---
+++ TeamManagementService/Model/Team.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamManagementService.Model
+{
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+
+        [Required]
+        public string TeamName { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
