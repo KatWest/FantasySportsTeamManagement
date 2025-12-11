011ddc95fcf3959478009943b87b0fbc7a2f2ab1
mpope124 <122325510+mpope124@users.noreply.github.com>
2025-12-10 20:43:46 -0800

    Got API to intereface with DB
    DB is now working! Working on changes to make it more pretty.

--- PerformanceTrackingService/Program.cs
+++ PerformanceTrackingService/Program.cs
@@ -7,36 +7,35 @@ builder.Services.AddDbContext<PerformanceContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddCors(Options =>
+
+
+builder.Services.AddCors(options =>
 {
-    Options.AddPolicy("AllowAll",
-        builder =>
+    options.AddPolicy("AllowAll",
+        policy =>
         {
-            builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
         });
 });
+
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseRouting();
 app.UseCors("AllowAll");
-
 app.UseAuthorization();
 
 app.MapControllers();
 
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<PerformanceContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 app.Run();
