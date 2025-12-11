7bf62f578c070dbc37add2511a3cf304a84fd8ef
mpope124 <122325510+mpope124@users.noreply.github.com>
2025-12-10 20:43:46 -0800

    Got API to intereface with DB
    DB is now working! Working on changes to make it more pretty.

--- PerformanceTrackingService/Controllers/PlayerStatsController.cs
+++ PerformanceTrackingService/Controllers/PlayerStatsController.cs
@@ -24,33 +24,12 @@ namespace PerformanceTrackingService.Controllers
             return await _context.PlayerStats.ToListAsync();
         }
 
-        // GET Player Stats by Player ID
-        [HttpGet("player/{playerId}")]
-        public async Task<ActionResult<IEnumerable<PlayerStat>>> GetByPlayer(int playerId)
-        {
-            var stats = await _context.PlayerStats
-                .Where(s => s.PlayerId == playerId)
-                .OrderByDescending(s => s.GameDate)
-                .ToListAsync();
-
-            if (!stats.Any())
-            { 
-                return NotFound();
-            }
-
-            else 
-            {
-                return stats; 
-            }
-            
-        }
-
+        // DTO for the simulate request
         public class SimulationRequest
         {
             public List<int> PlayerIds { get; set; } = new();
         }
 
-        // POST: api/PlayerStats/simulate
         [HttpPost("simulate")]
         public async Task<ActionResult<IEnumerable<PlayerStat>>> Simulate([FromBody] SimulationRequest request)
         {
@@ -59,76 +38,84 @@ namespace PerformanceTrackingService.Controllers
                 return BadRequest("At least one playerId is required.");
             }
 
-            var newStats = new List<PlayerStat>();
-
-            foreach (var playerId in request.PlayerIds)
+            try
             {
-                var stat = GenerateRandomFootballStats(playerId);
-                stat.FantasyPoints = CalculateFantasyPoints(stat);
+                var newStats = new List<PlayerStat>();
 
-                _context.PlayerStats.Add(stat);
-                newStats.Add(stat);
+                foreach (var playerId in request.PlayerIds)
+                {
+                    var stat = GenerateRandomFootballStats(playerId);
+                    stat.FantasyPoints = CalculateFantasyPoints(stat);
+
+                    _context.PlayerStats.Add(stat);
+                    newStats.Add(stat);
+                }
+
+                await _context.SaveChangesAsync();
+
+                // TODO: Notify Leaderboard service when it exists.
+                return Ok(newStats);
+            }
+            catch (Exception ex)
+            {
+                var inner = ex.InnerException?.Message ?? ex.Message;
+                return StatusCode(500, $"Server error: {inner}");
             }
 
-            await _context.SaveChangesAsync();
-
-            // TODO: Notify Leaderboard service when it exists.
-
-            return Ok(newStats);
         }
 
-       
-        // Made a method to generate random football stats
+
         private PlayerStat GenerateRandomFootballStats(int playerId)
         {
-            // random stats for football to be generated for each player
-            int passingYards = _random.Next(0, 401); 
-            int passingTd = _random.Next(0, 5);
+            int passingYards = _random.Next(0, 401);
+            int passingTouchdowns = _random.Next(0, 5);
             int interceptions = _random.Next(0, 3);
+
             int rushingYards = _random.Next(0, 151);
-            int rushingTd = _random.Next(0, 3);
+            int rushingTouchdowns = _random.Next(0, 3);
+
             int receptions = _random.Next(0, 11);
-            int receivingYds = _random.Next(0, 151);
-            int receivingTd = _random.Next(0, 3);
+            int receivingYards = _random.Next(0, 151);
+            int receivingTouchdowns = _random.Next(0, 3);
+
             int fumblesLost = _random.Next(0, 2);
 
             return new PlayerStat
             {
                 PlayerId = playerId,
                 GameDate = DateTime.UtcNow,
+
                 PassingYards = passingYards,
-                PassingTouchdowns = passingTd,
+                PassingTouchdowns = passingTouchdowns,
                 InterceptionsThrown = interceptions,
+
                 RushingYards = rushingYards,
-                RushingTouchdowns = rushingTd,
+                RushingTouchdowns = rushingTouchdowns,
+
                 Receptions = receptions,
-                ReceivingYards = receivingYds,
-                ReceivingTouchdowns = receivingTd,
+                ReceivingYards = receivingYards,
+                ReceivingTouchdowns = receivingTouchdowns,
+
                 FumblesLost = fumblesLost
             };
         }
 
-        // How the scoring will be done in our fantasy program.
-        // 1 pt / 25 passing yds, 4 per pass TD, -2 per INT
-        // 1 pt / 10 rush/rec yds, 6 per rush/rec TD, 1 per reception, -2 per fumble lost
-        // Since I don't play fantasy football, I did get some help from the internet to help with scoring.
         private int CalculateFantasyPoints(PlayerStat s)
         {
-            // Starting points with 0.
-            var points = 0.0;
+            double points = 0;
 
-            //Calculating the points for each player.
             points += s.PassingYards / 25.0;
             points += s.PassingTouchdowns * 4;
             points -= s.InterceptionsThrown * 2;
+
             points += s.RushingYards / 10.0;
             points += s.RushingTouchdowns * 6;
+
             points += s.ReceivingYards / 10.0;
             points += s.ReceivingTouchdowns * 6;
-            points += s.Receptions * 1;    // PPR
+            points += s.Receptions * 1;
             points -= s.FumblesLost * 2;
 
-            // returning the points for each player.
             return (int)Math.Round(points);
         }
     }
